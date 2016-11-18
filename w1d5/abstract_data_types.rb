ecf67b66173e375e462d799cbd261d15db91ef79
Katarina Rossi <katarina.n.rossi@gmail.com>
2016-11-17 23:39:15 -0800

    Implements Map class using 2D array

--- w1d5/abstract_data_types.rb
+++ w1d5/abstract_data_types.rb
@@ -33,3 +33,42 @@ class Queue
     @queue.dup
   end
 end
+
+class Map
+
+  def initialize
+    @map = Array.new { [] }
+  end
+
+  def assign(key, value)
+    previous_vals = lookup(key)
+    if previous_vals.nil?
+      @map << [key, [value]]
+    else
+      new_values = previous_vals << value
+      pair_idx = @map.find_index([key, previous_vals])
+      @map[pair_idx] = [key, new_values]
+    end
+  end
+
+  def lookup(key)
+    pair_idx = lookup_index(key)
+    return nil if pair_idx.nil?
+    @map[pair_idx].last
+  end
+
+  def remove(key)
+    pair_idx = lookup_index(key)
+    return nil if pair_idx.nil?
+    @map.delete_at(pair_idx)
+  end
+
+  def lookup_index(key)
+    @map.find_index { |pair| pair.first == key }
+  end
+
+  def show
+    @map.dup
+  end
+
+end

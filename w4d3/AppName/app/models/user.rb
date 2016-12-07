2bbf39e24066660cd9a1868a76e6eef417422284
Katarina Rossi <katarina.n.rossi@gmail.com>
2016-12-06 22:32:39 -0800

    Make small fixes

--- w4d3/AppName/app/models/user.rb
+++ w4d3/AppName/app/models/user.rb
@@ -1,40 +1,37 @@
 class User < ActiveRecord::Base
   attr_reader :password
 
-  validates :username, :session_token, presence: true
+  validates :username, presence: true
+  validates :session_token, presence: true, uniqueness: true
   validates :password_digest, presence: { message: "Password can't be blank" }
   validates :password, length: { minimum: 6, allow_nil: true }
 
   before_validation :ensure_session_token
 
-  def password=(password)
-   @password = password
-   self.password_digest = BCrypt::Password.create(password)
-  end
-
-  def find_by_credentials(username, password)
+  def self.find_by_credentials(username, password)
     user = User.find_by(username: username)
     return nil if user.nil?
 
     user.password_digest.is_password?(password) ? user : nil
-    end
   end
 
-  def password_digest
-    BCrypt::Password.new(super)
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
   end
 
-  def generate_session_token
+  def self.generate_session_token
     SecureRandom::urlsafe_base64
   end
 
   def reset_session_token!
     self.session_token = SecureRandom::urlsafe_base64
     self.save!
+    self.session_token
   end
 
   def ensure_session_token
-    self.session_token ||= generate_session_token
+    self.session_token ||= User.generate_session_token
   end
 
 end

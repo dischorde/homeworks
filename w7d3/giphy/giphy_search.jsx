ae75a91109b941d8eef26581c41e1477b0bd232e
Katarina Rossi <katarina.n.rossi@gmail.com>
2017-01-03 21:49:03 -0800

    Complete phase 1 of giphys homework

---
+++ w7d3/giphy/giphy_search.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+import Root from './components/root';
+import { receiveSearchGiphys, fetchSearchGiphys } from './actions/giphy_actions.js';
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  window.fetchSearchGiphys = fetchSearchGiphys;
+  const store = configureStore();
+  window.store = store;
+  window.receiveSearchGiphys = receiveSearchGiphys;
+});
